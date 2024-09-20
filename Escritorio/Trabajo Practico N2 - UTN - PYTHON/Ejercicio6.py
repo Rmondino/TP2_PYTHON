88a49b3f661ef8428603a05aef8d1d646da59936
Ramiro Hugas <150353453+Rmondino@users.noreply.github.com>
2024-09-20 08:47:52 -0300

    Create Ejercicio6.py

---
+++ Escritorio/Trabajo Practico N2 - UTN - PYTHON/Ejercicio6.py
@@ -0,0 +1,5 @@
+#De la siguiente cadena “La lluvia en Mendoza es escasa” indique cual es el tamaño 
+#de la cadena es decir su número de caracteres.
+cad="La lluvia en Mendoza es escasa"
+longitud_cad=len(cad) #La funcion len retorna el numero de elementos de una secuencia.
+print("El tamaño de la cadena es de ",longitud_cad,"caracteres")
